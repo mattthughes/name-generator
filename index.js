635b1f91ed69d249d58762bfb377836241a7b39e
Matt <118008508+mattthughes@users.noreply.github.com>
2026-06-22 16:18:43 +0000

    Added basic boilerplate for name generator

---
+++ index.js
@@ -0,0 +1,9 @@
+const names = [];
+
+const randomName = document.getElementById("name-generator")
+const randomBtn = document.getElementById("random-btn")
+
+
+randomBtn.addEventListener("click", function(){
+    console.log("button clicked")
+})
