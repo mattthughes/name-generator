339f1d90102cb48112ed350fddb32691b40918d2
Matt <118008508+mattthughes@users.noreply.github.com>
2026-06-24 14:14:23 +0000

    Added slot logic to create maxium amount of names allowed

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ const limit = document.getElementById("slots")
 
 const results = document.getElementById("results")
 
-randomBtn.addEventListener("click", function(){
-    names.push(randomName.value)
-    results.innerHTML = names
-    randomName.value = ""
+randomBtn.addEventListener("click", function () {
+    let sum = Number(limit.value)
+    if (names.length < sum) {
+        names.push(randomName.value)
+        results.innerHTML = names
+        randomName.value = ""
+    }
+    
+    console.log(names)
+
+    console.log(sum)
 })
