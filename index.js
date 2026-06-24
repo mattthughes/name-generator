cecdb8e40cc357d1fa287a3393e3516a1a9e5f04
Matt <118008508+mattthughes@users.noreply.github.com>
2026-06-24 16:32:01 +0000

    Added cleaner random names results

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const names = [];
 const randomName = document.getElementById("name-generator")
 const randomBtn = document.getElementById("random-btn")
 const limit = document.getElementById("slots")
-
-
+const bannedNames = ["Matt Hughes", "Darcey Padwick"]
 const results = document.getElementById("results")
 
+function cleanName(name) {
+    return name.split("(")[0].trim();
+}
+
 randomBtn.addEventListener("click", function () {
+    const name = randomName.value.trim();
     let sum = Number(limit.value)
-    if (names.length < sum) {
-        names.push(randomName.value)
+    if (bannedNames.includes(name) || bannedNames.includes(name)) {
+        results.innerHTML = `Please remove ${name}`
+    }
+    else if (names.length < sum) {
+        names.push(name)
         results.innerHTML = names
         randomName.value = ""
     }
