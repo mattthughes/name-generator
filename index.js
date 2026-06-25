e64877d89887a9c0980efc7c199e85a049c39d20
Matt <118008508+mattthughes@users.noreply.github.com>
2026-06-25 20:33:11 +0000

    Added random name winners logic

--- index.js
+++ index.js
@@ -11,16 +11,20 @@ function cleanName(name) {
 }
 
 const isNameBanned = (name) => {
+    // Setting is banned to false so that when you loop through the name array it can check if banned names are there otherwise it will set every single name to a banned name
     let isBanned = false;
     bannedNames.forEach(bannedName => {
+        // if the name is a perfect match
         if (name.toLowerCase() === bannedName.toLowerCase()) {
+            // only set to true if the banned name is found
             isBanned = true
         }
     })
     return isBanned;
 }
-
+// looping through the pasted names
 const pastedNames = (str, sum) => {
+    randomName.value = ""
     const pastedRows = str.split(/[,\n]+/)
     pastedRows.forEach(row => {
         const name = cleanName(row.trim());
@@ -28,14 +32,15 @@ const pastedNames = (str, sum) => {
 
 
         if (isNameBanned(name)) {
-            results.innerHTML = `Please remove ${name}`
-
+            // finding the index, if the user and name match 
             const index = names.findIndex(user => user.toLowerCase() === name.toLowerCase())
+            // if the index is -1 then remove that element from the array
             if (index !== -1) {
                 names.splice(index, 1)
             }
         }
-        else if (names.length < sum) {
+        // if names is less than sum then add the names to the array
+        else if (name !== "") {
             names.push(name)
 
         }
@@ -43,10 +48,48 @@ const pastedNames = (str, sum) => {
 
 }
 
+const winnersList = (array, numberOfWinners) => {
+    const nameCopy = [...array]
+    const winners = []
+
+    if (numberOfWinners > nameCopy.length) {
+        results.innerHTML = "Not enough names in the list, please add more"
+        return []
+    }
+
+    for (let i = 0; i < numberOfWinners; i++) {
+        if (nameCopy.length === 0) break
+        const randomNames = Math.floor(Math.random() * nameCopy.length)
+        const chosenNames = nameCopy.splice(randomNames, 1).pop()
+        winners.push(chosenNames)
+    }
+
+    return winners
+}
+
 randomBtn.addEventListener("click", function () {
+
+    // set the sum variable but change it to a number as by default this is a string
     let sum = Number(limit.value)
-    pastedNames(randomName.value, sum)
-    results.innerHTML = names.join("<br> ")
+
+    // set the string variable to the randomName input field and it's value, use the sum variable to set the limit
+    pastedNames(randomName.value)
+    // using join to add all elements of the string together and seperate with a break tag, to make sure each element is on it's own line
+
+    // set the input field to empty once the name has been added
     randomName.value = ""
-    console.log(names)
+
+    const finalWinners = winnersList(names, sum)
+
+
+    if (finalWinners.length === 0) {
+        results.innerHTML = `Please add some names`
+    } else  {
+
+
+        results.innerHTML = finalWinners
+        console.log(finalWinners)
+    }
+
+
 })
